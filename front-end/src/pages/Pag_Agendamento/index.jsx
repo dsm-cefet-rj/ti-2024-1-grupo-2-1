18ee32946d0bedc22ead81d5de4681277a04ebdd
Felipe Nogueira <felipenogueiralidi@gmail.com>
2024-03-20 14:26:19 -0300

    Primeiro agendamento

---
+++ front-end/src/pages/Pag_Agendamento/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import HeaderMain from "../../components/Header";
+import Footer from "../../components/Footer/indexfooter";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './style.css';
+
+export const Agendamento = () => {
+    const [date, setDate] = useState(new Date());
+
+    const onChange = date => {
+        setDate(date);
+    };
+
+    return (
+        <div>
+            <HeaderMain></HeaderMain>
+            <span className="introducao"><strong>Agende sua visita</strong></span>
+            <span className="line"></span>
+
+            <div className="Calendar">
+                <Calendar
+                    onChange={onChange}
+                    value={date}
+                />
+            </div>
+
+            <div className="content"></div>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Agendamento;
