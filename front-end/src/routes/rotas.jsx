47400fe73da7ca139234d386a8f8a01430e5bd33
Yzhak Zalmom <yzhakzalmom@gmail.com>
2024-03-20 14:52:15 -0300

    Fim do teste com git fetch

--- front-end/src/routes/rotas.jsx
+++ front-end/src/routes/rotas.jsx
@@ -1,21 +1,21 @@
 import { Fragment } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Favoritos } from "../pages/Favoritos";
 import { Detalhamento } from "../pages/Detalhamento";
 import { Main } from "../pages/Principal";
-
-
+import Agendamento from "../pages/Pag_Agendamento";
 
 export const AppRouter = () => {
-    return( 
-        <Router>
-            <Fragment>
-                <Routes>
-                    <Route path="/principal" element={<Main/>}/>
-                    <Route path="/favoritos" element={<Favoritos/>}/>
-                    <Route path="/detalhamento" element={<Detalhamento/>}/>
-                </Routes>
-            </Fragment>    
-        </Router>
-    );
+  return (
+    <Router>
+      <Fragment>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/favoritos" element={<Favoritos />} />
+          <Route path="/detalhamento" element={<Detalhamento />} />
+          <Route path="/agendamento" element={<Agendamento />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  );
 };
