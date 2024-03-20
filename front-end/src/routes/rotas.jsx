7d8cb421b0f3a294d8f2b0596181e312052154b2
Flavio Alecio <flaviotubaroni@gmail.com>
2024-03-20 10:26:28 -0300

    Adicionado pagina e rota de detalhamento

--- front-end/src/routes/rotas.jsx
+++ front-end/src/routes/rotas.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Favoritos } from "../pages/Favoritos";
+import { Detalhamento } from "../pages/Detalhamento";
 
 
 
@@ -10,6 +11,7 @@ export const AppRouter = () => {
             <Fragment>
                 <Routes>
                     <Route path="/favoritos" element={<Favoritos/>}/>
+                    <Route path="/detalhamento" element={<Detalhamento/>}/>
                 </Routes>
             </Fragment>    
         </Router>
