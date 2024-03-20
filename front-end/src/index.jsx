00f965bcd1e5d70fa55de22702ecab4503134900
FlaviouSs <flaviotubaroni@gmail.com>
2024-03-20 18:07:41 -0300

    Adicionado divs na pagina de detalhamento e estilização das divs de detalhamento

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -12,5 +12,3 @@ root.render(
      
   </React.StrictMode>
 );
-
-
--- front-end/src/pages/Detalhamento/index.jsx
+++ front-end/src/pages/Detalhamento/index.jsx
@@ -1,23 +1,33 @@
-import HeaderMain from "../../components/HeaderMain"
-import Footer from "../../components/Footer"
+import HeaderMain from "../../components/HeaderMain";
+import Footer from "../../components/Footer";
+import "./index.css";
 
 export const Detalhamento = () => {
+
+    const url = "https://via.placeholder.com/350";
 
     return (
         <div>
             <HeaderMain/>
                 <h1>Nome do Animal</h1>
-                <div>
-                    <h2>Localização do Animal:</h2>
-                    <h2>Publicador:</h2>
-                    <h2>Acessos à página:</h2>
-                </div>
-                <div>
-                    <h2>História do Animal</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                <div className="divBody">
+                    <div className="divEsquerda">
+                        <img src={url} alt="Imagem do Animal"></img>
+                    </div>
+                    <div className="divDireita">
+                        <div>
+                            <h2>Localização do Animal:</h2>
+                            <h2>Publicador:</h2>
+                            <h2>Acessos à página:</h2>
+                        </div>
+                        <div>
+                            <h2>História do Animal</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                        </div>
+                    </div>
                 </div>
             <Footer/>
         </div>
