41f38b9514979acbdd9bea48c3bb5bd207ff1bb7
Yzhak Zalmom <yzhakzalmom@gmail.com>
2024-03-20 12:23:27 -0300

    Adicionando o bootstrap a public/index.html para que esteja disponível em todas as páginas

--- front-end/src/pages/Favoritos/index.jsx
+++ front-end/src/pages/Favoritos/index.jsx
@@ -1,11 +1,14 @@
-import HeaderMain from "../../components/HeaderMain"
-import Footer from "../../components/Footer"
+import HeaderMain from "../../components/HeaderMain";
+import Footer from "../../components/Footer";
 
 export const Favoritos = () => {
-    return(
-        <div>
-            <HeaderMain></HeaderMain>
-            <Footer></Footer>
-        </div>
-    )
-}
+  return (
+    <div>
+      <HeaderMain></HeaderMain>
+      <div>
+        <h1>Seus Favoritos</h1>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
