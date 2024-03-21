9c130c6699491e546ff4913325ae94621bfd4ab0
Flavio Alecio <flaviotubaroni@gmail.com>
2024-03-21 00:38:01 -0300

    Update de estilização na página de Detalhamento

--- front-end/src/pages/Detalhamento/index.jsx
+++ front-end/src/pages/Detalhamento/index.jsx
@@ -4,28 +4,32 @@ import "./index.css";
 
 export const Detalhamento = () => {
 
-    const url = "https://via.placeholder.com/350";
+    const url = "https://via.placeholder.com/750x500";
 
     return (
         <div>
             <HeaderMain/>
-                <h1>Nome do Animal</h1>
                 <div className="divBody">
                     <div className="divEsquerda">
                         <img src={url} alt="Imagem do Animal"></img>
                     </div>
                     <div className="divDireita">
-                        <div>
+                        <div className="nomeAnimal">
+                            <h1>Nome do Animal</h1>
+                        </div>
+                        <div className="statusAnimal">
                             <h2>Localização do Animal:</h2>
                             <h2>Publicador:</h2>
                             <h2>Acessos à página:</h2>
                         </div>
-                        <div>
-                            <h2>História do Animal</h2>
+                        <div className="historiaAnimal">
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, eaque!</p>
+                        </div>
+                        <div className="botaoAdotar">
+                            <input type="button" value="QUERO ADOTAR" id="botaoQueroAdotar"/>
                         </div>
                     </div>
                 </div>
