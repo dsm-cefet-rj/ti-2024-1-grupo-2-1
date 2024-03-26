985f8a68b5ec8ddfc8f82a770d49fa8186fed4ab
VnszinN <147839981+VnszinN@users.noreply.github.com>
2024-03-26 11:32:35 -0300

    Criação do objeto de teste animal

---
+++ front-end/src/components/Animal/animal.js
@@ -0,0 +1,85 @@
+
+
+const animal = {
+    1: {
+        id: 1,
+        img: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQOO0X7mMnoYz-e9Zdc6Pe6Wz7Ow1DcvhEiaex5aSv6QJDoCtcooqA7UUbjrphvjlIc",
+        nome: "Flavio",
+        tipo: "Cachorro",
+        porte: "Grande",
+        sexo: "Macho",
+        idade: "5 anos",
+        historia: "Flavio é um cachorro amável que foi encontrado vagando pelas ruas em busca de comida e abrigo. Ele tem uma personalidade gentil e brincalhona, e rapidamente conquista o coração de todos que o conhecem. Apesar de ter tido um passado difícil, Flavio é extremamente leal e está pronto para encontrar um lar amoroso onde possa receber todo o carinho e cuidado que merece."
+    },
+      2: {
+        id: 2,
+        img: "https://www.petz.com.br/blog/wp-content/uploads/2021/11/tipos-de-pitbull3-1280x720.jpg",
+        nome: "Vini",
+        tipo: "Cachorro",
+        porte: "Pequeno",
+        sexo: "Macho",
+        idade: "8 anos",
+        história: "Conheça o adorável Vini: Um cachorro de coração valente à espera de um lar amoroso! Vini chegou até nós após enfrentar muitos desafios em sua jornada pela vida. Com seus 8 anos de idade, ele exibe uma determinação e uma força de espírito que são verdadeiramente inspiradoras. Apesar das dificuldades que enfrentou, Vini mantém um coração cheio de amor e uma disposição alegre para todos que cruzam seu caminho. Ele adora brincar, dar longos passeios e desfrutar de momentos de carinho e afeto. Vini é do tipo que ilumina qualquer ambiente com sua presença contagiante e seu espírito brincalhão. Se você está em busca de um amigo leal e cheio de energia para compartilhar suas aventuras, Vini é o companheiro perfeito. Ao adotá-lo, você estará não apenas oferecendo a ele um lar seguro e acolhedor, mas também ganhando um amigo fiel para a vida toda. Venha conhecer o adorável Vini e descubra como ele pode trazer alegria e amor para sua vida todos os dias!"
+      },
+      3: {
+        id: 3,
+        img: "https://ih1.redbubble.net/image.1677539366.0714/aps,504x498,medium,transparent-pad,600x600,f8f8f8.jpg",
+        nome: "Felipe",
+        tipo: "Gato",
+        porte: "Grande",
+        sexo: "Macho",
+        idade: "7 anos",
+        história: "Conheça o encantador Felipe: Um gato majestoso à procura de um lar caloroso! Felipe chegou ao nosso abrigo após ser encontrado vagando pelas ruas, exibindo uma elegância e calma que capturou nossa atenção imediatamente. Com seus 7 anos de idade, ele é um verdadeiro cavalheiro felino, sempre pronto para receber um carinho suave ou compartilhar um momento tranquilo ao sol. Felipe é do tipo que prefere uma vida serena, desfrutando de longas sonecas e observando o mundo ao seu redor com olhos sábios. Se você procura por um companheiro tranquilo e amoroso para trazer serenidade ao seu lar, Felipe é a escolha perfeita. Ao adotá-lo, você estará oferecendo a ele um lugar seguro e acolhedor para passar seus dias, enquanto ele retribui com todo o seu charme e afeto. Venha conhecer o adorável Felipe e descubra como sua presença pode transformar sua vida para sempre!"
+      },
+      4: {
+        id: 4,
+        img: "https://t.ctcdn.com.br/QzjtRMp1djLnIW2I5XgPS825Ox4=/640x360/smart/i615361.jpeg",
+        nome: "Yzhak",
+        tipo: "Gato",
+        porte: "Médio",
+        sexo: "Macho",
+        idade: "9 anos"
+      },
+      5: {
+        id: 5,
+        img: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQOO0X7mMnoYz-e9Zdc6Pe6Wz7Ow1DcvhEiaex5aSv6QJDoCtcooqA7UUbjrphvjlIc",
+        nome: "Flavio",
+        tipo: "Cachorro",
+        porte: "Grande",
+        sexo: "Macho",
+        idade: "5 anos",
+        historia: "Flavio é um cachorro amável que foi encontrado vagando pelas ruas em busca de comida e abrigo. Ele tem uma personalidade gentil e brincalhona, e rapidamente conquista o coração de todos que o conhecem. Apesar de ter tido um passado difícil, Flavio é extremamente leal e está pronto para encontrar um lar amoroso onde possa receber todo o carinho e cuidado que merece."
+    },
+      6: {
+        id: 6,
+        img: "https://www.petz.com.br/blog/wp-content/uploads/2021/11/tipos-de-pitbull3-1280x720.jpg",
+        nome: "Vini",
+        tipo: "Cachorro",
+        porte: "Pequeno",
+        sexo: "Macho",
+        idade: "8 anos",
+        história: "Conheça o adorável Vini: Um cachorro de coração valente à espera de um lar amoroso! Vini chegou até nós após enfrentar muitos desafios em sua jornada pela vida. Com seus 8 anos de idade, ele exibe uma determinação e uma força de espírito que são verdadeiramente inspiradoras. Apesar das dificuldades que enfrentou, Vini mantém um coração cheio de amor e uma disposição alegre para todos que cruzam seu caminho. Ele adora brincar, dar longos passeios e desfrutar de momentos de carinho e afeto. Vini é do tipo que ilumina qualquer ambiente com sua presença contagiante e seu espírito brincalhão. Se você está em busca de um amigo leal e cheio de energia para compartilhar suas aventuras, Vini é o companheiro perfeito. Ao adotá-lo, você estará não apenas oferecendo a ele um lar seguro e acolhedor, mas também ganhando um amigo fiel para a vida toda. Venha conhecer o adorável Vini e descubra como ele pode trazer alegria e amor para sua vida todos os dias!"
+      },
+      7: {
+        id: 7,
+        img: "https://ih1.redbubble.net/image.1677539366.0714/aps,504x498,medium,transparent-pad,600x600,f8f8f8.jpg",
+        nome: "Felipe",
+        tipo: "Gato",
+        porte: "Grande",
+        sexo: "Macho",
+        idade: "7 anos",
+        história: "Conheça o encantador Felipe: Um gato majestoso à procura de um lar caloroso! Felipe chegou ao nosso abrigo após ser encontrado vagando pelas ruas, exibindo uma elegância e calma que capturou nossa atenção imediatamente. Com seus 7 anos de idade, ele é um verdadeiro cavalheiro felino, sempre pronto para receber um carinho suave ou compartilhar um momento tranquilo ao sol. Felipe é do tipo que prefere uma vida serena, desfrutando de longas sonecas e observando o mundo ao seu redor com olhos sábios. Se você procura por um companheiro tranquilo e amoroso para trazer serenidade ao seu lar, Felipe é a escolha perfeita. Ao adotá-lo, você estará oferecendo a ele um lugar seguro e acolhedor para passar seus dias, enquanto ele retribui com todo o seu charme e afeto. Venha conhecer o adorável Felipe e descubra como sua presença pode transformar sua vida para sempre!"
+      },
+      8: {
+        id: 8,
+        img: "https://t.ctcdn.com.br/QzjtRMp1djLnIW2I5XgPS825Ox4=/640x360/smart/i615361.jpeg",
+        nome: "Yzhak",
+        tipo: "Gato",
+        porte: "Médio",
+        sexo: "Macho",
+        idade: "9 anos"
+      }
+  
+}
+
+export default animal
