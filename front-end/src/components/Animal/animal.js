b3f96b2b3d4f622edea8873aa72323c8837424b1
VnszinN <147839981+VnszinN@users.noreply.github.com>
2024-03-28 00:35:15 -0300

    Conserto da margem da grid do bootstrap e modificação de alguns componentes

--- front-end/src/components/Animal/animal.js
+++ front-end/src/components/Animal/animal.js
@@ -43,44 +43,65 @@ const animal = {
       },
       5: {
         id: 5,
-        img: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQOO0X7mMnoYz-e9Zdc6Pe6Wz7Ow1DcvhEiaex5aSv6QJDoCtcooqA7UUbjrphvjlIc",
-        nome: "Flavio",
+        img: "https://pt.vecteezy.com/foto/5857332-engracado-retrato-de-fofo-corgi-cao-ao-ar-livre",
+        nome: "Luna",
         tipo: "Cachorro",
-        porte: "Grande",
-        sexo: "Macho",
-        idade: "5 anos",
-        história: "Flavio é um cachorro amável que foi encontrado vagando pelas ruas em busca de comida e abrigo. Ele tem uma personalidade gentil e brincalhona, e rapidamente conquista o coração de todos que o conhecem. Apesar de ter tido um passado difícil, Flavio é extremamente leal e está pronto para encontrar um lar amoroso onde possa receber todo o carinho e cuidado que merece."
+        porte: "Médio",
+        sexo: "Fêmea",
+        idade: "3 anos",
+        história: "Luna é uma cachorrinha muito dócil que adora brincar e receber carinho. Ela foi encontrada abandonada na rua e desde então tem sido cuidada com muito amor. Luna está em busca de um lar acolhedor onde possa ser amada e retribuir todo o afeto que tem para oferecer."
     },
-      6: {
+    6: {
         id: 6,
-        img: "https://www.petz.com.br/blog/wp-content/uploads/2021/11/tipos-de-pitbull3-1280x720.jpg",
-        nome: "Vini",
+        img: "https://pt.vecteezy.com/foto/3018949-close-up-portrait-of-tricolor-australian-shepherd-dog-deitado-na-mesa-de-um-parque-olho-natural-ao-dele",
+        nome: "Branco",
         tipo: "Cachorro",
         porte: "Pequeno",
         sexo: "Macho",
-        idade: "8 anos",
-        história: "Conheça o adorável Vini: Um cachorro de coração valente à espera de um lar amoroso! Vini chegou até nós após enfrentar muitos desafios em sua jornada pela vida. Com seus 8 anos de idade, ele exibe uma determinação e uma força de espírito que são verdadeiramente inspiradoras. Apesar das dificuldades que enfrentou, Vini mantém um coração cheio de amor e uma disposição alegre para todos que cruzam seu caminho. Ele adora brincar, dar longos passeios e desfrutar de momentos de carinho e afeto. Vini é do tipo que ilumina qualquer ambiente com sua presença contagiante e seu espírito brincalhão. Se você está em busca de um amigo leal e cheio de energia para compartilhar suas aventuras, Vini é o companheiro perfeito. Ao adotá-lo, você estará não apenas oferecendo a ele um lar seguro e acolhedor, mas também ganhando um amigo fiel para a vida toda. Venha conhecer o adorável Vini e descubra como ele pode trazer alegria e amor para sua vida todos os dias!"
-      },
-      7: {
+        idade: "2 anos",
+        história: "Branco é um cachorrinho alegre e brincalhão que adora correr e explorar novos lugares. Ele é muito sociável e se dá bem com outros animais. Encontrado abandonado na rua, Branco espera encontrar uma família amorosa que possa dar a ele todo o carinho e cuidado que merece."
+    },
+    7: {
         id: 7,
-        img: "https://ih1.redbubble.net/image.1677539366.0714/aps,504x498,medium,transparent-pad,600x600,f8f8f8.jpg",
-        nome: "Felipe",
-        tipo: "Gato",
+        img: "https://pt.vecteezy.com/foto/17622035-belo-retrato-de-close-up-de-um-cachorro",
+        nome: "Mel",
+        tipo: "Cachorro",
         porte: "Grande",
-        sexo: "Macho",
-        idade: "7 anos",
-        história: "Conheça o encantador Felipe: Um gato majestoso à procura de um lar caloroso! Felipe chegou ao nosso abrigo após ser encontrado vagando pelas ruas, exibindo uma elegância e calma que capturou nossa atenção imediatamente. Com seus 7 anos de idade, ele é um verdadeiro cavalheiro felino, sempre pronto para receber um carinho suave ou compartilhar um momento tranquilo ao sol. Felipe é do tipo que prefere uma vida serena, desfrutando de longas sonecas e observando o mundo ao seu redor com olhos sábios. Se você procura por um companheiro tranquilo e amoroso para trazer serenidade ao seu lar, Felipe é a escolha perfeita. Ao adotá-lo, você estará oferecendo a ele um lugar seguro e acolhedor para passar seus dias, enquanto ele retribui com todo o seu charme e afeto. Venha conhecer o adorável Felipe e descubra como sua presença pode transformar sua vida para sempre!"
-      },
-      8: {
-        id: 8,
-        img: "https://t.ctcdn.com.br/QzjtRMp1djLnIW2I5XgPS825Ox4=/640x360/smart/i615361.jpeg",
-        nome: "Yzhak",
-        tipo: "Gato",
-        porte: "Médio",
-        sexo: "Macho",
-        idade: "9 anos",
-        história: "Yzhak é um gato adorável que foi resgatado das ruas quando era apenas um filhote. Ele teve uma infância difícil, mas graças aos cuidados amorosos de seus protetores, ele se transformou em um gato carinhoso e brincalhão. Yzhak adora brincar com bolinhas de papel e tirar sonecas aconchegantes em seu cantinho favorito. Ele está ansioso para encontrar um lar amoroso onde possa receber todo o carinho e atenção que merece."
-      }
+        sexo: "Fêmea",
+        idade: "4 anos",
+        história: "Mel é uma cadela doce e amorosa que adora brincar e receber atenção. Ela foi resgatada de um abrigo onde estava há algum tempo esperando por uma família. Mel está pronta para encontrar um lar onde possa ser feliz e fazer seus donos felizes também."
+    },
+    // Continuar com mais objetos seguindo o mesmo padrão...
+    8: {
+      id: 8,
+      img: "https://img.freepik.com/fotos-gratis/feche-o-gato-fofo-dentro-de-casa_23-2148882585.jpg?size=626&ext=jpg&ga=GA1.1.1459418701.1707737255&semt=sph",
+      nome: "Luma",
+      tipo: "Gato",
+      porte: "Médio",
+      sexo: "Fêmea",
+      idade: "2 anos",
+      história: "Luna é uma gatinha adorável que adora brincar com bolinhas de lã e tomar longos cochilos ao sol. Ela foi encontrada abandonada em uma caixa na rua e desde então tem sido cuidada com muito amor. Luna está em busca de um lar onde possa receber todo o carinho e atenção que merece."
+  },
+  9: {
+      id: 9,
+      img: "https://img.freepik.com/fotos-gratis/lindo-gato-peludo-relaxando-dentro-de-casa_23-2150679180.jpg?size=626&ext=jpg&ga=GA1.1.1459418701.1707737255&semt=sph",
+      nome: "Spike",
+      tipo: "Gato",
+      porte: "Grande",
+      sexo: "Macho",
+      idade: "5 anos",
+      história: "Spike é um gato tranquilo e afetuoso que adora passar o tempo observando os pássaros pela janela. Ele foi resgatado de um abrigo onde estava esperando por uma segunda chance. Spike espera encontrar um lar onde possa relaxar e desfrutar da companhia de sua família para sempre."
+  },
+  10: {
+      id: 10,
+      img: "https://img.freepik.com/fotos-gratis/close-vertical-de-um-lindo-gato-europeu-de-pelo-curto_181624-34587.jpg?size=626&ext=jpg&ga=GA1.1.1459418701.1707737255&semt=sph",
+      nome: "Bella",
+      tipo: "Gato",
+      porte: "Pequeno",
+      sexo: "Fêmea",
+      idade: "3 anos",
+      história: "Bella é uma gatinha brincalhona e cheia de energia que adora explorar todos os cantos da casa. Ela foi resgatada das ruas quando ainda era filhote e desde então tem sido uma companheira leal e amorosa. Bella está em busca de um lar onde possa continuar espalhando alegria e receber todo o amor que merece."
+  }
   
 }
 
--- front-end/src/components/PetCards/index.jsx
+++ front-end/src/components/PetCards/index.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 /*import { IoMdHeart } from "react-icons/io";
 import { IoIosHeartEmpty } from "react-icons/io";*/
 import './style.css'
 import Card from 'react-bootstrap/Card';
 //import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
-import SmoothScroll from 'smooth-scroll';
 
 
 const PetCards = ({ animais, showLink = true }) => {
 
     const id = animais.id;
     const navigate = useNavigate();
-    //<div className="pet-card">
+    // <div className="pet-card">
     //     <img className="img_pet" src={animais.img}></img>
 
     //     <h2>{animais.nome}</h2>
@@ -32,7 +31,7 @@ const PetCards = ({ animais, showLink = true }) => {
     }
 
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card className="card-animal"style={{ width: '18rem' }}>
             <Card.Img variant="top" src={animais.img} />
             <Card.Body>
                 <Card.Title>{animais.nome}</Card.Title>
@@ -44,6 +43,7 @@ const PetCards = ({ animais, showLink = true }) => {
                 {showLink && <button className="info" onClick={handleClick} >Detalhes</button>}
             </Card.Body>
         </Card>
+        
     )
 }
 
