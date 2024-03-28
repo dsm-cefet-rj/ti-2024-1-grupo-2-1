6c817df2330657dafd9ffa8a46c0eff3cbe35ea4
Flavio Alecio <flaviotubaroni@gmail.com>
2024-03-28 12:24:44 -0300

    Estilização da página Registro de Adoção

--- front-end/src/pages/RegistroAdocao/index.jsx
+++ front-end/src/pages/RegistroAdocao/index.jsx
@@ -14,7 +14,20 @@ export const RegistroAdocao = () => {
                         <span className="sublinha-favoritos"></span>
                     </div>
                     <div className="div-principal">
-
+                        <div className="div-informacoes">
+                            <div className="div-info-esquerda">
+                                <img src="https://placehold.co/300x300" alt="Imagem" id="teste"></img>
+                            </div>
+                            <div className="div-info-direita">
+                                <img src="https://placehold.co/100x100" alt="Imagem"></img>
+                            </div>
+                        </div>
+                        <div className="div-responsabilidade-adocao">
+                            <div className="titulo-responsabilidade">
+                                <h3>Registro de Adoção</h3>
+                                <span className="sublinha-responsabilidade"></span>
+                            </div>
+                        </div>
                     </div>
                </div>
             <Footer/>
