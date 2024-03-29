bdf3d7f24597707a1f86787260fbc95106006d59
Flavio Alecio <flaviotubaroni@gmail.com>
2024-03-29 17:30:36 -0300

    Estilização da página Registro de Adoção, responsividade ainda prejudicada

--- front-end/src/pages/RegistroAdocao/index.jsx
+++ front-end/src/pages/RegistroAdocao/index.jsx
@@ -5,6 +5,8 @@ import Footer from "../../components/Footer";
 
 export const RegistroAdocao = () => {
 
+    const linkTermoAdocao = "https://pt.scribd.com/document/331088569/Termo-de-Adocao-de-Caes";
+
     return (
         <div>
             <HeaderMain/>
@@ -46,7 +48,74 @@ export const RegistroAdocao = () => {
                             <div className="titulo-responsabilidade">
                                 <h3>Responsabilidade de Adoção</h3>
                                 <span className="sublinha-responsabilidade"></span>
+                                <div className="div-inputs-responsabilidade">
+                                    <div className="div-pergunta">
+                                        <span>
+                                            1. Quem será o responsável pelo cuidado do animal?
+                                        </span>
+                                        <div>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div className="div-pergunta">
+                                        <span>
+                                            2. Animais que não tem acesso à rua vivem 15 anos ou mais.
+                                            Você está pronto para esse compromisso e responsabilidade de longo prazo?
+                                        </span>
+                                        <div>
+                                            <span>Sim</span>
+                                            <input type="radio" name="q2"/>
+                                            <span>Não</span>
+                                            <input type="radio" name="q2"/>
+                                        </div>
+                                    </div>
+                                    <div className="div-pergunta">
+                                        <span>
+                                            3. Os animais precisam de cuidados com higiene, boa alimentação e
+                                            atendimento veterinário. Você tem como providenciar isso?
+                                        </span>
+                                        <div>
+                                            <span>Sim</span>
+                                            <input type="radio" name="q3"/>
+                                            <span>Não</span>
+                                            <input type="radio" name="q3"/>
+                                        </div>
+                                    </div>
+                                    <div className="div-pergunta">
+                                        <span>
+                                            4. Você já tem um médico veterinário?
+                                        </span>
+                                        <div>
+                                            <span>Sim</span>
+                                            <input type="radio" name="q4"/>
+                                            <span>Não</span>
+                                            <input type="radio" name="q4"/>
+                                        </div>
+                                    </div>
+                                    <div className="div-pergunta">
+                                        <span>
+                                            5. Em caso de emergência, tem como levar seu animal imediatamente
+                                            ao veterinário?
+                                        </span>
+                                        <div>
+                                            <span>Sim</span>
+                                            <input type="radio" name="q5"/>
+                                            <span>Não</span>
+                                            <input type="radio" name="q5"/>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+                        </div>
+                        <div className="div-documentos">
+                            <a href={linkTermoAdocao} target="_blank">Baixe aqui o termo de adoção</a>
+                            <div>
+                                <span>Anexe o termo de adoção, identidade e comprovante de residência: </span>
+                                <input type="file" multiple></input>
+                            </div>
+                        </div>
+                        <div className="div-btn-registrar-adocao">
+                            <input type="button" value="REGISTRAR ADOÇÃO" id="btn-registar-adocao" />
                         </div>
                     </div>
                </div>
