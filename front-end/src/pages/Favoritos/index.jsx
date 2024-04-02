0c09f261479ed7226e9e6158dac86c66ca441ea1
Yzhak Zalmom <yzhakzalmom@gmail.com>
2024-04-02 19:58:32 -0300

    Adicionando lógica de filtro na página de Favoritos

--- front-end/src/pages/Favoritos/index.jsx
+++ front-end/src/pages/Favoritos/index.jsx
@@ -10,6 +10,7 @@ import Paginacao from "../../components/Pagination";
 import { useState } from "react";
 import { useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
+import FilterFavoritos from "../../components/FilterFavoritos";
 
 export const Favoritos = () => {
   const { id } = useParams();
@@ -50,59 +51,7 @@ export const Favoritos = () => {
         <span className="title-favoritos">Seus Favoritos</span>
         <span className="sublinha-favoritos"></span>
       </div>
-      <form className="form-filtro-favoritos" action="">
-        <div>
-          <select name="tipo-animal" id="tipo-animal">
-            <option value="" disabled selected>
-              Tipo
-            </option>
-            <option value="cachorro">Cachorro</option>
-            <option value="gato">Gato</option>
-          </select>
-        </div>
-        <div>
-          <select name="porte-animal" id="porte-animal">
-            <option value="" disabled selected>
-              Porte
-            </option>
-            <option value="grande">Grande</option>
-            <option value="medio">Médio</option>
-            <option value="pequeno">Pequeno</option>
-          </select>
-        </div>
-        <div>
-          <select name="idade-animal" id="idade-animal">
-            <option value="" disabled selected>
-              Idade
-            </option>
-            <option value="filhote">Filhote</option>
-            <option value="adulto">Adulto</option>
-            <option value="idoso">Idoso</option>
-          </select>
-        </div>
-        <div>
-          <select name="bairro-animal" id="bairro-animal">
-            <option value="" disabled selected>
-              Bairro
-            </option>
-            <option value="maracana">Maracanã</option>
-            <option value="tijuca">Tijuca</option>
-            <option value="vila-isabel">Vila Isabel</option>
-          </select>
-        </div>
-        <div>
-          <select name="sexo-animal" id="sexo-animal">
-            <option value="" disabled selected>
-              Sexo
-            </option>
-            <option value="Macho">Macho</option>
-            <option value="Fêmea">Fêmea</option>
-          </select>
-        </div>
-        <button className="botao-search" type="submit">
-          Filtrar
-        </button>
-      </form>
+      <FilterFavoritos></FilterFavoritos>
       <div className="grade-container">
         <Grade>
           {itensAtuais.map((animal) => (
