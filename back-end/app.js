37cee597ec50a0f39c903cf7490b5ec7e58cf4e6
Flavio Alecio <flaviotubaroni@gmail.com>
2024-08-29 10:47:27 -0300

    Inicio da documentação da API Back-End usando o Swagger
    Co-authored-by: Felipe Nogueira <FelipeNoglidi@users.noreply.github.com>

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 const cors = require('cors');
 var logger = require('morgan');
 var passport = require('passport');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -44,8 +46,11 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(passport.initialize());
 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', indexRouter);
 app.use('/userDB', usersRouter);
 app.use('/animals', animalsRouter);
